3a342e4136d1b075727a3138085f13e5ab04c590
Michael Frye <michael@michaelfrye.me>
2016-12-09 19:18:43 -0800

    initial commit

---
+++ index.js
@@ -0,0 +1,55 @@
+var express = require('express');
+var path = require('path');
+var bodyParser = require('body-parser')
+var debug = require('debug')('index');
+
+var emailFinder = require('./lib/email-finder');
+
+var app = express();
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+var rootDir = path.resolve(__dirname);
+
+app.set('port', (process.env.PORT || 5000));
+
+// Configure jade as template engine
+app.set('views', rootDir + '/views');
+app.set('view engine', 'ejs');
+app.set("view options", {layout: false});
+
+// Parse the body
+// Warning: http://andrewkelley.me/post/do-not-use-bodyparser-with-express-js.html
+// parse application/json
+app.use(bodyParser.json())
+
+// Serve static content from "public" directory
+app.use(express.static(rootDir + '/public'));
+
+
+app.get('/', function(req, res){
+  res.render('index', {
+    GOOGLE_ANALYTICS_ID: process.env.GOOGLE_ANALYTICS_ID || ''
+  });
+});
+
+app.post('/find', function(req, res) {
+
+  var data = {
+    name: req.body.first_name + ' ' + req.body.last_name,
+    domain: req.body.domain
+  };
+
+  emailFinder(data)
+  .then(function (email) {
+    res.send({email: email});
+  })
+  .catch(function (err) {
+    res.status(500).send(err);
+  })
+});
+
+  // All set, start listening!
+app.listen(app.get('port'), function() {
+  console.log(`Server running at http://localhost:${app.get('port')}`);
+});
---
+++ public/js/main.js
@@ -0,0 +1,59 @@
+
+
+// On result
+function onResult() {
+
+}
+
+// On submit
+function onSubmit(e) {
+  var loadingCover = $('.loading-cover');
+  var result = $('#result');
+
+  e.preventDefault();
+
+  // Get data from form
+  var data = $(e.target).serializeArray().reduce(function(obj, item) {
+    obj[item.name] = item.value;
+    return obj;
+  }, {});
+
+  // Show loading screen
+  loadingCover.addClass('show');
+
+  // Clear old result
+  result.html('');
+
+  $('#result').html
+
+  $.ajax({
+    url: "/find",
+    method: "POST",
+    data: JSON.stringify(data),
+    contentType: "application/json; charset=utf-8",
+    dataType: "json",
+  })
+  .done(function(data) {
+    console.log(data);
+
+    // Hide loading screen
+    loadingCover.removeClass('show');
+
+    // Set result
+    $('#result').html('Success! The email is:' + data.email);
+  })
+  .fail(function (err) {
+
+    // Hide loading screen
+    loadingCover.removeClass('show');
+
+    // Set result
+    $('#result').html('There was a problem finding the email.');
+  });
+
+  return false;
+}
+
+var form = document.getElementById('email-form');
+
+form.addEventListener('submit', onSubmit);
