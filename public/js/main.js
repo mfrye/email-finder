fd2c097b80459b77e50408b24104de3eebb6d284
Michael Frye <michael@michaelfrye.me>
2016-12-12 22:38:20 -0800

    email capture for continued use

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,70 @@
 
+// Get sesssion data from localStorage
+var LOCAL_STORAGE_KEY = 'bp_session';
+var sessionData = window.localStorage.getItem(LOCAL_STORAGE_KEY);
+sessionData = sessionData ? JSON.parse(sessionData) : {submissions: 0, email: false};
+
+/*
+ * Check session
+ *
+ * - Only allow 3 email checks before asking for email
+ */
+function checkSession() {
+  if (!sessionData.email && sessionData.submissions >= 3) {
+    $('#modal1').modal('open');
+  }
+}
+
+/*
+ * On email modal submit handler
+ */
+function onEmailSubmit(e) {
+
+  e.preventDefault();
+
+  // Get data from form
+  var data = buildData(e.target);
+
+  if (!validate(data)) {
+    return
+  }
+
+  // Disable form
+  $('#email-capture input').attr('disabled', true);
+  $('#email-capture button').attr('disabled', true);
+
+  // $('#result').html;
+  data.type = 'Email Finder';
+
+  $.ajax({
+    url: "https://7umdo22ge3.execute-api.us-west-2.amazonaws.com/dev/email",
+    method: "POST",
+    data: JSON.stringify(data),
+    timeout: 20000,
+    contentType: "application/json; charset=utf-8",
+    dataType: "json",
+  })
+  .done(function(data) {
+
+    $('#email-capture input').attr('disabled', false);
+    $('#email-capture button').attr('disabled', false);
+
+    // Set that email has been submitted
+    sessionData.email = true;
+
+    window.localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(sessionData));
+
+    $('#modal1').modal('close');
+  })
+  .fail(function (err) {
+
+    $('#email-capture input').attr('disabled', false);
+    $('#email-capture button').attr('disabled', false);
+  });
+
+  return false;
+}
+
 /*
  * Validate the form
  *
@@ -78,6 +144,13 @@ function onSubmit(e) {
 
     // Set result
     $('#result').html('Success! The email is: ' + data.email);
+
+    // Count the number of submissions
+    sessionData.submissions++;
+
+    window.localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(sessionData));
+
+    checkSession();
   })
   .fail(function (err) {
 
@@ -95,11 +168,23 @@ function onSubmit(e) {
  * Initialize
  */
 function init() {
-  var form = document.getElementById('email-form');
 
-  form.addEventListener('submit', onSubmit);
+  $('#email-form').on('submit', onSubmit);
+  $('#email-capture').on('submit', onEmailSubmit);
 
   $(".button-collapse").sideNav();
+
+  $('.modal').modal({
+      dismissible: false, // Modal can be dismissed by clicking outside of the modal
+      opacity: .5, // Opacity of modal background
+      in_duration: 300, // Transition in duration
+      out_duration: 200, // Transition out duration
+      starting_top: '4%', // Starting top style attribute
+      ending_top: '10%', // Ending top style attribute
+    }
+  );
+
+  checkSession();
 }
 
-init();
+$(document).ready(init);
